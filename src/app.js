9596984b4f2abb6ef6ff020566446e41aa3eacd2
leorega <leonardoregazzoni@gmail.com>
2024-02-02 16:27:09 -0300

    Agrego funcionalidades a GraphQL

--- src/app.js
+++ src/app.js
@@ -16,15 +16,41 @@ const typeDefs = `#graphql
         allProducts: [Product]
         findProduct(name:String!): Product
     }
+
+    type Mutation {
+        addProduct (
+            name: String!
+            description: String
+            image: String
+            price: Float        
+        ): Product
+        editPrice (
+            name: String!
+            price: Float
+        ): Product
+    }
 `;
 
 const resolvers = {
     Query: {
         productsCount: () => Product.collection.countDocuments(),
-        allProducts: () => Product.find({}),
+        allProducts: async () => {
+            return await Product.find({});
+        },
         findProduct: (root, args) => {
             const { name } = args;
             return Product.findOne({ name });
+        },
+    },
+    Mutation: {
+        addProduct: (root, args) => {
+            const product = new Product({ ...args });
+            return product.save();
+        },
+        editPrice: async (root, args) => {
+            const product = await Product.findOne({ name: args.name });
+            product.price = args.price;
+            return product.save();
         },
     },
 };
